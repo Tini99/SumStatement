33319628ea1e3b02332bdd2dd8143efd4f7919cb
Tini Gisa <1814759@nzse.ac.nz>
2021-06-14 19:39:20 +1200

    Add project files.

---
+++ SumStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, sum;
+            sum = 0;
+
+            for (Number1 = 1; Number1 <= 500; Number1 = Number1 + 1)
+            {
+                if (Number1 % 5 == 0)
+                {
+                    Console.WriteLine(Number1);
+                    sum = sum + Number1;
+                }
+            }
+            Console.WriteLine("sum of first 500 numbers those are divided by 5 are =" + sum.ToString());
+        }
+    }
+}
